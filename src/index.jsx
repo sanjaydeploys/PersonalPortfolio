f5863f9dcec31fe7f8ce242af0630cde1af0511b
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-29 23:05:48 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'; // Import from 'react-dom/client'
 import { AppProvider } from './context/AppContext';
 import AppRouter from './Router';
+import { Provider } from 'react-redux';
 
 
 const rootElement = document.getElementById('root');
 const appRoot = createRoot(rootElement);
 
 appRoot.render(
+
+   <Provider>
   <React.StrictMode>
    
     <AppProvider>
@@ -17,5 +20,5 @@ appRoot.render(
     </AppProvider>
   
   </React.StrictMode>,
-
+</Provider>
 );
