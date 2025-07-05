d7ef653e6e9899dec844f59379c1a606295216d0
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-07-06 00:07:32 +0530

    Update homePage.jsx

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -65,7 +65,7 @@ const homePage = memo(() => {
       setSsrHtml(document.documentElement.outerHTML);
       setLoading(false);
     } else {
-      fetch('https://js2h38fca8.execute-api.ap-south-1.amazonaws.com/prod')
+      fetch('https://gj48940cgb.execute-api.ap-south-1.amazonaws.com/prod')
         .then((res) => {
           if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
           return res.text();
