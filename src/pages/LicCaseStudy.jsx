c085cb8ece407661cf1cc5209c0f0d4a15186b6d
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-07-05 15:08:58 +0530

    Update LicCaseStudy.jsx

--- src/pages/LicCaseStudy.jsx
+++ src/pages/LicCaseStudy.jsx
@@ -65,7 +65,7 @@ const KileshwarMahadevPage = memo(() => {
       setSsrHtml(document.documentElement.outerHTML);
       setLoading(false);
     } else {
-      fetch('https://gj48940cgb.execute-api.ap-south-1.amazonaws.com/prod/lic-case-study')
+      fetch('https://gj48940cgb.execute-api.ap-south-1.amazonaws.com/prod')
         .then((res) => {
           if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
           return res.text();
